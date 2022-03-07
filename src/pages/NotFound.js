8183af5143fb9074b5a5fbe55b3c52ed8f094af3
Marcos Campos <marxmyers@icloud.com>
2022-03-07 16:10:26 -0300

    Adicionando Requisito 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>notFound</h1>
+      </div>
+    );
+  }
+}
+
+export default Album;
