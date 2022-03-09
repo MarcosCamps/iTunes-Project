f2ab27395fdca76bb7c11e92f710ae3e39a31cd6
Marcos Campos <marxmyers@icloud.com>
2022-03-09 13:31:01 -0300

    Adicionando requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,13 +9,21 @@ class Search extends React.Component {
 
     this.state = {
       searchButton: true,
+      albums: [],
+      loading: false,
+      artistName: '',
+      inputArtist: '',
     };
 
-    this.buttonArtist = this.buttonArtist.bind(this);
+    this.buttonSearch = this.buttonSearch.bind(this);
+    this.searchAlbums = this.searchAlbums.bind(this);
   }
 
-  buttonArtist(event) {
+  buttonSearch(event) {
     const characterMin = 2;
+    this.setState({
+      artistName: event.target.value,
+    });
     if (event.target.value.length >= characterMin) {
       this.setState({
         searchButton: false,
@@ -21,30 +31,78 @@ class Search extends React.Component {
     }
   }
 
+  async searchAlbums(event) {
+    const { artistName } = this.state;
+    event.preventDefault();
+    this.setState({ loading: true });
+    const results = await searchAlbumsAPI(artistName);
+    this.setState({
+      albums: results,
+      loading: false,
+      artistName: '',
+      inputArtist: artistName,
+    });
+    console.log(results);
+  }
+
   render() {
-    const { searchButton } = this.state;
+    const { searchButton, artistName, loading, albums, inputArtist } = this.state;
     return (
       <div data-testid="page-search">
         <h1>Search</h1>
         <Header />
-        <form>
-          <input
-            data-testid="search-artist-input"
-            type="text"
-            name="artist-name"
-            placeholder="Digite a banda ou o artista"
-            onChange={ this.buttonArtist }
-          />
-          <button
-            data-testid="search-artist-button"
-            type="submit"
-            name="searchButton"
-            // onClick={ }
-            disabled={ searchButton }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading
+          ? <p>Carregando...</p>
+          : (
+            <>
+              <div>
+                <form>
+                  <input
+                    data-testid="search-artist-input"
+                    type="text"
+                    name="artistName"
+                    value={ artistName }
+                    placeholder="Digite a banda ou o artista"
+                    onChange={ this.buttonSearch }
+                  />
+                  <button
+                    data-testid="search-artist-button"
+                    type="submit"
+                    name="searchButton"
+                    onClick={ this.searchAlbums }
+                    disabled={ searchButton }
+                  >
+                    Pesquisar
+                  </button>
+                </form>
+                <h2>
+                  {`Resultado de álbuns de: ${inputArtist}`}
+                </h2>
+              </div>
+              {albums.length <= 0
+                ? <p>Nenhum álbum foi encontrado</p>
+                : (
+                  <section>
+                    {albums.map((album, index) => (
+                      <div key={ `album-${index}` }>
+                        <Link
+                          to={ `/album/${album.collectionId}` }
+                          data-testid={ `link-to-album-${album.collectionId}` }
+                        >
+                          <img
+                            src={ album.artworkUrl100 }
+                            alt=""
+                          />
+                          <h3>{ album.collectionName }</h3>
+                          <span>{ album.artistName }</span>
+                        </Link>
+                        {console.log(album.collectionId)}
+                      </div>
+                    ))}
+                  </section>
+                )}
+            </>
+          )}
       </div>
     );
   }
