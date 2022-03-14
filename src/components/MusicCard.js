7a4efa18bc9866c90b851af5afe6fe17de1ada1b
Marcos Campos <marxmyers@icloud.com>
2022-03-14 13:26:10 -0300

    Corrigindo lint css

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { addSong } from '../services/favoriteSongsAPI';
-import './card.css';
 
 class MusicCard extends React.Component {
   constructor(props) {
